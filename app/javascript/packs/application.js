6ba78c7f8a6a7005f17b60c0539b9dd20b963393
t-masa0724mi <s100.iizuka.masami@gmail.com>
2021-08-26 17:54:31 +0900

    プロフィール画像追加実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../main")
+require("src/profile_image_upload");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
 
+  mount_uploader :profile_image, ProfileImageUploader
+
   def update_without_current_password(params, *options)
 
     if params[:password].blank? && params[:password_confirmation].blank?
